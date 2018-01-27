b47e22c3dacf4b33e32a6bfa1cbfa67466f61d82
Mathias Born <mathias.born@gmx.de>
2018-01-27 22:28:48 +0100

    no comment

--- xet/interfaces.h
+++ xet/interfaces.h
@@ -131,8 +131,6 @@ class Token
 {
 public:
 	virtual ~Token() {};
-	//	virtual void addedToPage(PPage&) {};
-	//	virtual void addedToTypeSetter() {};
 };
 
 typedef std::shared_ptr<Token> PToken;
@@ -151,16 +149,45 @@ private:
 	std::u32string m_text;
 };
 
-class Callable: public Text
+class Callable: public Token
 {
 public:
 	Callable() {};
-	Callable(Tokens&& block, std::u32string const& text): m_block{std::move(block)}, Text{text} {};
+	Callable(Tokens&& block): m_block{std::move(block)} {};
+
+	virtual Tokens result() = 0;
 protected:
 	Tokens m_block;
+private:
+	Tokens m_result;
 };
 
-glue, penalty, ...
+class Actor: public Token
+{
+public:
+	virtual void addedToPage(PPage&) {};
+	virtual void addedToTypeSetter() {};
+};
+
+class Glue: public Token
+{
+
+private:
+	xet::Size m_width;
+	xet::Size m_stretchability;
+	xet::Size m_shrinkability;
+};
+
+class Penalty: public Token
+{
+public:
+	static constexpr int pinf = +1000;
+	static constexpr int ninf = -1000;
+private:
+	int m_value;
+	xet::Size m_width;
+};
+
 
 /*
 class Text: public Token
